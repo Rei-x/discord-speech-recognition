0d3195a189bb1a57f7730e138465df515e948211
Rei-666 <wszyzdgo@gmail.com>
2021-05-12 20:40:03 +0200

    Refactor wit.ai api client

--- tests/witAI.test.ts
+++ tests/witAI.test.ts
@@ -8,7 +8,7 @@ chai.use(chaiAsPRosmied);
 const expect = chai.expect;
 const speechRecognitionSamples = [['https://cdn.discordapp.com/attachments/838767598778843149/841360475631779861/Hello_my_name_is_John.wav', 'hello my name is john']];
 
-describe('witAI test', function() {
+describe.only('witAI test', function() {
   this.timeout(4000);
   const [url, text] = speechRecognitionSamples[0];
   it('Speech recognition', async function() {
@@ -22,6 +22,26 @@ describe('witAI test', function() {
     const badAudioBuffer = Buffer.from('test');
     return expect(resolveSpeechWithWITAI(badAudioBuffer, {
       key: WITAI_KEY,
-    })).to.be.rejected;
+    })).to.be.rejectedWith('Wrong request data');
+  });
+  it('Empty request body throws error', function() {
+    const emptyBuffer = Buffer.from('');
+    return expect(resolveSpeechWithWITAI(emptyBuffer, {
+      key: WITAI_KEY,
+    })).to.be.rejectedWith('Api error, code: 400');
+  });
+  it('Empty token throws error', async function() {
+    const [url] = speechRecognitionSamples[0];
+    const audioBuffer = await wavUrlToBuffer(url);
+    return expect(resolveSpeechWithWITAI(audioBuffer, {
+      key: '',
+    })).to.be.rejectedWith('wit.ai API key wasn\'t specified.');
+  });
+  it('Bad token throws error', async function() {
+    const [url] = speechRecognitionSamples[0];
+    const audioBuffer = await wavUrlToBuffer(url);
+    return expect(resolveSpeechWithWITAI(audioBuffer, {
+      key: 'd',
+    })).to.be.rejectedWith('Api error, code: 400');
   });
 });
