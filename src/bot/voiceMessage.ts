6494a2be4717807fd9f32bffcc4c339b6ef0b798
Rei-666 <wszyzdgo@gmail.com>
2021-05-15 20:51:11 +0200

    Better documenation for voice message

--- src/bot/voiceMessage.ts
+++ src/bot/voiceMessage.ts
@@ -1,40 +1,63 @@
 import {Client, Guild, GuildMember, User, VoiceChannel} from 'discord.js';
-
+import wav from 'wav';
 interface VoiceMessageData {
-    duration: number;
-    audioBuffer: Buffer;
-    content?: string,
-    error?: string,
-    author: User,
+  duration: number;
+  audioBuffer: Buffer;
+  content?: string,
+  error?: string,
+  author: User,
 }
 
 export class VoiceMessage {
-    channel: VoiceChannel;
-    content?: string;
-    author: User;
-    duration: number;
-    audioBuffer: Buffer;
-    client: Client;
-    error?: string;
-    /**
-     * Voice message, it is emited `speech` event
-     * @param client
-     * @param data
-     * @param channel
-     */
-    constructor(client: Client, data: VoiceMessageData, channel: VoiceChannel) {
-      this.client = client;
-      this.channel = channel;
-      this.author = data.author;
-      this.audioBuffer = data.audioBuffer;
-      this.duration = data.duration;
-      this.content = data?.content;
-      this.error = data?.error;
-    }
-    get member(): GuildMember|null {
-      return this.guild ? this.guild.member(this.author) || null : null;
-    }
-    get guild(): Guild|null {
-      return this.channel.guild || null;
-    }
+  channel: VoiceChannel;
+  /**
+   * Speech to text translation
+   */
+  content?: string;
+  author: User;
+  /**
+   * Duration in seconds
+   */
+  duration: number;
+  /**
+   * PCM mono 48k audio data
+   */
+  audioBuffer: Buffer;
+  client: Client;
+  /**
+   * If there was any error during handling speech event, this will be set
+   */
+  error?: string;
+  guild: Guild;
+  member: GuildMember | null;
+  /**
+   * Voice message, it is emited `speech` event
+   * @param client
+   * @param data
+   * @param channel
+   * @private
+   */
+  constructor(client: Client, data: VoiceMessageData, channel: VoiceChannel) {
+    this.client = client;
+    this.channel = channel;
+    this.author = data.author;
+    this.audioBuffer = data.audioBuffer;
+    this.duration = data.duration;
+    this.content = data?.content;
+    this.error = data?.error;
+    this.guild = this.channel.guild;
+    this.member = this.guild.member(this.author);
+  }
+  /**
+   * Saves audio to .wav file
+   * @param filename File directory, for example: `./test.wav`
+   */
+  saveToFile(filename: string): void {
+    const outputFile = new wav.FileWriter(filename, {
+      sampleRate: 48000,
+      channels: 2,
+    });
+    outputFile.write(this.audioBuffer);
+    outputFile.end();
+  }
 }
