a50b932a950f18855bbbc648b2febe1974de292b
Rei-666 <wszyzdgo@gmail.com>
2021-05-16 18:04:39 +0200

    Update export names

--- src/index.ts
+++ src/index.ts
@@ -3,4 +3,4 @@ export {speech, voiceJoin} from './events';
 export {VoiceMessage} from './bot/voiceMessage';
 export {resolveSpeechWithGoogleSpeechV2, GoogleSpeechV2Options} from './speechRecognition/googleV2';
 export {resolveSpeechWithWITAI, WITAIOptions} from './speechRecognition/witAI';
-export {wavUrlToBuffer, getDurationFromStereoBuffer as getDurationFromBuffer} from './utils/audio';
+export {wavUrlToBuffer, getDurationFromMonoBuffer} from './utils/audio';
