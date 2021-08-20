fb3596d7e210ec4355c1fac9e927ff62f8dad6dc
Rei-x <wszyzdgo@gmail.com>
2021-08-20 09:42:53 +0000

    Change eslint config, add prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,27 +1,28 @@
 module.exports = {
-  'env': {
-    'browser': true,
-    'commonjs': true,
-    'es2021': true,
+  env: {
+    browser: true,
+    commonjs: true,
+    es2021: true,
   },
-  'parser': '@typescript-eslint/parser',
-  'extends': [
-    'google',
-    'eslint:recommended',
-    'plugin:@typescript-eslint/eslint-recommended',
-    'plugin:@typescript-eslint/recommended',
+  parser: "@typescript-eslint/parser",
+  extends: [
+    "airbnb-typescript/base",
+    "eslint:recommended",
+    "plugin:@typescript-eslint/eslint-recommended",
+    "plugin:@typescript-eslint/recommended",
+    "prettier",
   ],
-  'plugins': [
-    '@typescript-eslint',
-  ],
-  'parserOptions': {
-    'ecmaVersion': 12,
+  plugins: ["@typescript-eslint", "prettier"],
+  parserOptions: {
+    ecmaVersion: 12,
+    project: "./tsconfig.json",
   },
-  'rules': {
-    'require-jsdoc': 0,
-    'valid-jsdoc': 0,
-    'semi': 2,
-    'max-len': 0,
-    'no-prototype-builtins': 0,
+  rules: {
+    "require-jsdoc": 0,
+    "valid-jsdoc": 0,
+    "prettier/prettier": "error",
+    semi: 2,
+    "max-len": 0,
+    "no-prototype-builtins": 0,
   },
 };
