713d411a5b8fe5f2c09f3bd75ad12e945cff48f7
Rei-x <wszyzdgo@gmail.com>
2021-10-02 14:06:02 +0000

    Fix slashCommands example

--- examples/slashCommands/deployCommands.js
+++ examples/slashCommands/deployCommands.js
@@ -1,6 +1,6 @@
 const { SlashCommandBuilder } = require("@discordjs/builders");
 const { REST } = require("@discordjs/rest");
-const { Routes } = require("discord-api-types");
+const { Routes } = require("discord-api-types/v9");
 const { clientId, guildId, token } = require("./config.json");
 
 const commands = [
