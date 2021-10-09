8984ad31b7863fbc1244acf2edc8d49c9a4725da
Rei-x <wszyzdgo@gmail.com>
2021-10-09 09:49:42 +0000

    Fix simpleBot example

--- examples/simpleBot/index.js
+++ examples/simpleBot/index.js
@@ -1,8 +1,13 @@
-const { Client, Message } = require("discord.js");
+const { Client, Intents } = require("discord.js");
+const { joinVoiceChannel } = require("@discordjs/voice");
 const { addSpeechEvent } = require("discord-speech-recognition");
 
 const client = new Client({
-  intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_VOICE_STATES],
+  intents: [
+    Intents.FLAGS.GUILDS,
+    Intents.FLAGS.GUILD_VOICE_STATES,
+    Intents.FLAGS.GUILD_MESSAGES,
+  ],
 });
 addSpeechEvent(client);
 
@@ -20,6 +25,10 @@ client.on("messageCreate", (msg) => {
 
 client.on("speech", (msg) => {
   msg.author.send(msg.content);
+});
+
+client.on("ready", () => {
+  console.log("Ready!");
 });
 
 client.login("token");
