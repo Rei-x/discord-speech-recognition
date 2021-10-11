dc494bc7bbc0e73eac4adbd89642ae89eacfd138
Rei-x <wszyzdgo@gmail.com>
2021-10-11 18:32:48 +0000

    Fix firing multiple speech recognitions

--- src/bot/events/voiceJoin.ts
+++ src/bot/events/voiceJoin.ts
@@ -1,6 +1,23 @@
-import { getVoiceConnection } from "@discordjs/voice";
+import { getVoiceConnection, VoiceConnection } from "@discordjs/voice";
 import { Client } from "discord.js";
 import { SpeechOptions } from "../speechOptions";
+
+/**
+ * It's a bit hacky solution to check if speech handler has been already attached to connection receiver
+ * It does it by checking if in the listeners of speaking map exists function with the same name as function
+ * which handles speech event
+ * @param connection
+ * @returns
+ */
+const isSpeechHandlerAttachedToConnection = (
+  connection: VoiceConnection
+): boolean => {
+  return Boolean(
+    connection.receiver.speaking
+      .listeners("start")
+      .find((func) => func.name === "handleSpeechEventOnConnectionReceiver")
+  );
+};
 
 export default (client: Client, speechOptions: SpeechOptions): void => {
   client.on("voiceStateUpdate", (_old, newVoiceState) => {
@@ -10,7 +27,7 @@ export default (client: Client, speechOptions: SpeechOptions): void => {
       newVoiceState.channel.guild.id,
       speechOptions.group
     );
-    if (connection)
+    if (connection && !isSpeechHandlerAttachedToConnection(connection))
       client.emit(
         "voiceJoin",
         getVoiceConnection(newVoiceState.channel.guild.id, speechOptions.group)
