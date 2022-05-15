cf6e598f35be3be049a9a9339c34faa8f14418af
Rei-x <wszyzdgo@gmail.com>
2022-05-15 10:52:15 +0000

    Fix tests

--- tests/sampleData.ts
+++ tests/sampleData.ts
@@ -1,7 +1,7 @@
 export default {
   normal: {
     url: "https://cdn.discordapp.com/attachments/838767598778843149/841360475631779861/Hello_my_name_is_John.wav",
-    text: "hello my name is",
+    text: "my name is",
   },
   vulgarism: {
     url: "https://cdn.discordapp.com/attachments/838767598778843149/891340727878029342/What_the_fuck_are_you_doing_right_now.wav",
